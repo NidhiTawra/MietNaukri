3aa464b26bc79b05c6c78e369161056ca5c97460
Ashwin Saxena <ashwin.saxena24@gmail.com>
2018-06-26 17:16:43 +0530

    Optimized frontend

---
+++ header.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="style.css">
+    <script src="script.js"></script>
+</head>
+<body>
+
+<div id="mySidenav" class="sidenav">
+    <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
+    <a href="#">About</a>
+    <a href="#">Services</a>
+    <a href="#">Clients</a>
+    <a href="#">Contact</a>
+</div>
+
+<nav class="navbar navbar-inverse" id="main">
+    <div class="container-fluid">
+        <div class="navbar-header">
+            <span style="font-size:30px;cursor:pointer" onclick="openNav()"  class="navbar-brand" >&#9776;</span>
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+
+            <a class="navbar-brand" href="index.php">MIET JOB PORTAL</a>
+        </div>
+        <div class="collapse navbar-collapse" id="myNavbar">
+            <ul class="nav navbar-nav">
+                <li class="active"><a href="#">Home</a></li>
+                <li class="dropdown">
+                    <a class="dropdown-toggle" data-toggle="dropdown" href="#">Page 1 <span class="caret"></span></a>
+                    <ul class="dropdown-menu">
+                        <li><a href="#">Page 1-1</a></li>
+                        <li><a href="#">Page 1-2</a></li>
+                        <li><a href="#">Page 1-3</a></li>
+                    </ul>
+                </li>
+                <li><a href="#">Page 2</a></li>
+                <li><a href="#">Page 3</a></li>
+            </ul>
+            <ul class="nav navbar-nav navbar-right">
+                <li><a href="trysignup.php"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
+                <li><a href="trylogin.php"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+            </ul>
+        </div>
+    </div>
+</nav>
+
+</span>
+
+
+</div>
---
+++ trysignup.php
@@ -0,0 +1,19 @@
+<?php
+include_once "header.php";
+?>
+<div class="split left" id="left">
+  <div class="centered">
+   <a href="signup.php"><img src="images/img_avatar2.png" alt="SIGN-UP as Employee">
+   <h2>SIGN UP AS A JOB SEEKER</h2></a>
+  </div>
+</div>
+
+<div class="split right" id="right">
+  <div class="centered">
+   <a href="signup.php">  <img src="images/img_avatar.png" alt="SIGN-UP as Employer">
+    <h2>SIGN UP AS A JOB PROVIDER</h2></a>
+  </div>
+</div>
+     
+</body>
+</html> 
